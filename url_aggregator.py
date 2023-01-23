deb3c149dfd495b7c16ff785146c58aae0224bf2
blankdean <blankdean@gmail.com>
2023-01-23 11:48:04 -0500

    add error handling and exe file

--- url_aggregator.py
+++ url_aggregator.py
@@ -1,31 +1,53 @@
+import sys
 import openpyxl
 from googlesearch import search
 
-# Open the Excel spreadsheet
-excel_file = "companies.xlsx"
-wb = openpyxl.load_workbook(excel_file)
-sheet = wb["Sheet1"]
-
-# Loop through the rows in the spreadsheet
-for row in range(2, sheet.max_row + 1):
-    # Get the company name and URL from the spreadsheet
-    company_name = sheet.cell(row, 1).value
-    company_url = sheet.cell(row, 2).value
-    
-    # If the company URL is not already filled in
-    if not company_url:
+def fill_urls(excel_file):
+    # Open the Excel spreadsheet
+    wb = openpyxl.load_workbook(excel_file)
+    sheet = wb["Sheet1"]
+    print(f"Opening Spreadsheet {excel_file}")
+    modified = False
+    # Loop through the rows in the spreadsheet
+    for row in range(2, sheet.max_row + 1):
+        # Get the company name and URL from the spreadsheet
+        company_name = sheet.cell(row, 1).value
+        company_url = sheet.cell(row, 2).value
         
-        # Do a Google search for the company name
-        # Docs: https://python-googlesearch.readthedocs.io/en/latest/
-        for url in search(query=company_name, num=1, stop=1, pause=2):
-            company_url = url
-        
-        print(company_name)
-        print(f"Populating {excel_file} at row {row}: {company_url}")
-        print('----------')
+        # If the company URL is not already filled in
+        if not company_url:
+            
+            # Do a Google search for the company name
+            # Docs: https://python-googlesearch.readthedocs.io/en/latest/
+            for url in search(query=company_name, num=1, stop=1, pause=2):
+                company_url = url
+            
+            print(company_name)
+            print(f"Populating {excel_file} at row {row}: {company_url}")
+            print('----------')
 
-        # Update the company URL in the spreadsheet
-        sheet.cell(row, 2).value = company_url
+            # Update the company URL in the spreadsheet
+            sheet.cell(row, 2).value = company_url
 
-# Save the changes to the spreadsheet
-wb.save("companies.xlsx")
+            modified = True
+
+    if modified:
+        # Save the changes to the spreadsheet
+        print(f"Saving changes to {excel_file}")
+        wb.save(excel_file)
+    else:
+        print(f"No changes to spreadsheet. All urls filled in {excel_file}.")
+
+    wb.close()
+
+if __name__=="__main__":
+    excel_file = "companies.xlsx"
+
+    try:
+        fill_urls(excel_file)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    input("Press Enter to exit...")
+    sys.exit()
+
