aa7d3cd14c47c7a7dc961b9f149889a7020aee73
blankdean <blankdean@gmail.com>
2023-01-23 22:30:37 -0500

    save to spreadsheet even after error

--- url_aggregator.py
+++ url_aggregator.py
@@ -2,12 +2,10 @@ import sys
 import openpyxl
 from googlesearch import search
 
-def fill_urls(excel_file):
+def fill_urls(excel_file, wb, urls):
     # Open the Excel spreadsheet
-    wb = openpyxl.load_workbook(excel_file)
     sheet = wb["Sheet1"]
     print(f"Opening Spreadsheet {excel_file}")
-    modified = False
     # Loop through the rows in the spreadsheet
     for row in range(2, sheet.max_row + 1):
         # Get the company name and URL from the spreadsheet
@@ -21,33 +19,31 @@ def fill_urls(excel_file):
             # Docs: https://python-googlesearch.readthedocs.io/en/latest/
             for url in search(query=bytes(company_name, 'utf-8'), num=1, stop=1, pause=2):
                 company_url = url
-            
+
             print(company_name)
             print(f"Populating {excel_file} at row {row}: {company_url}")
             print('----------')
 
             # Update the company URL in the spreadsheet
             sheet.cell(row, 2).value = company_url
-
-            modified = True
-
-    if modified:
-        # Save the changes to the spreadsheet
-        print(f"Saving changes to {excel_file}")
-        wb.save(excel_file)
-    else:
-        print(f"No changes to spreadsheet. All urls filled in {excel_file}.")
-
-    wb.close()
+            urls.append(company_url)
+    
 
 if __name__=="__main__":
     excel_file = "companies.xlsx"
 
+    wb = openpyxl.load_workbook(excel_file)
+    urls = []
     try:
-        fill_urls(excel_file)
+        modified_wb = fill_urls(excel_file, wb, urls)
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    if len(urls) > 0:
+        # save if modified even if it fails
+        print(f"Saving {len(urls)} urls to {excel_file}")
+        wb.save(excel_file)
+
+    wb.close()
     input("Press Enter to exit...")
     sys.exit()
-
